afaa9d639a3e3c580eb6b53f52a3e632bbd0650f
Carlos <Carlos@LAPTOP-9J2JFI70>
2017-10-05 21:25:19 +0200

    Commit inicial

--- index.php
+++ index.php
@@ -6,32 +6,12 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="windows-1252">
+        <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
         <?php
-        echo "proba</br>";
-      $valor1=1;
-		$valor2=2;
-		
-		//Suma
-		echo "1+2= ".($valor1+$valor2)."<br/>";
-		
-		//Resta
-		echo "1-2= ".($valor1-$valor2)."<br/>";
-		
-		//Producte
-		echo "1*2= ".($valor1*$valor2)."<br/>";
-		
-		//Divisió
-		echo "1/2= ".($valor1/$valor2)."<br/>";
-		
-		//Divisió
-		echo "1/2= ".($valor1/$valor2)."<br/>";
-                
-                //Divisió
-		echo "1/2= ".($valor1/$valor2)."<br/>";
+        echo "proba";
         ?>
     </body>
 </html>
