706d17e24bf4ec816df9c4cb58951af75cc60104
Carlos <Carlos@LAPTOP-9J2JFI70>
2017-10-05 21:00:00 +0200

    Commit inicial

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "proba</br>";
+      $valor1=1;
+		$valor2=2;
+		
+		//Suma
+		echo "1+2= ".($valor1+$valor2)."<br/>";
+		
+		//Resta
+		echo "1-2= ".($valor1-$valor2)."<br/>";
+		
+		//Producte
+		echo "1*2= ".($valor1*$valor2)."<br/>";
+		
+		//Divisió
+		echo "1/2= ".($valor1/$valor2)."<br/>";
+		
+		//Divisió
+		echo "1/2= ".($valor1/$valor2)."<br/>";
+                
+                //Divisió
+		echo "1/2= ".($valor1/$valor2)."<br/>";
+        ?>
+    </body>
+</html>
