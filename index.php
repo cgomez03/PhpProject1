bb0d6ab0d23d639e1d7dd14fc8da8b48c70e15b3
Carlos <Carlos@LAPTOP-9J2JFI70>
2017-10-05 21:26:51 +0200

    Commit añadir metodos

--- index.php
+++ index.php
@@ -12,6 +12,28 @@ and open the template in the editor.
     <body>
         <?php
         echo "proba";
+        
+        $valor1=1;
+		$valor2=2;
+		
+		//Suma
+		echo "1+2= ".($valor1+$valor2)."<br/>";
+		
+		//Resta
+		echo "1-2= ".($valor1-$valor2)."<br/>";
+		
+		//Producte
+		echo "1*2= ".($valor1*$valor2)."<br/>";
+		
+		//Divisió
+		echo "1/2= ".($valor1/$valor2)."<br/>";
+		
+		//Divisió
+		echo "1/2= ".($valor1/$valor2)."<br/>";
+                
+                //Divisió
+		echo "1/2= ".($valor1/$valor2)."<br/>";
+        ?>
         ?>
     </body>
 </html>
